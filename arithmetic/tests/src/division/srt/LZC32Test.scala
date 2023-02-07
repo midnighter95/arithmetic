db7187c57d55d2448231c8c67166696a50aa8ae8
Yanqi Yang <shxxyyq0000@qq.com>
2023-02-07 23:31:25 +0800

    add LZC32

--- arithmetic/tests/src/division/srt/LZC32Test.scala
+++ arithmetic/tests/src/division/srt/LZC32Test.scala
@@ -1,39 +1,40 @@
 package division.srt
-import Chisel.PriorityEncoder
-import chiseltest.ChiselUtestTester
-import utest.TestSuite
-import utils.LZC8
+
 import chisel3._
-import chiseltest._
-import utest._
+import utils._
+import chiseltest.{ChiselUtestTester, _}
+import utest.{TestSuite, _}
 
 import scala.util.Random
 
-object LZC8Test extends TestSuite with ChiselUtestTester{
+object LZC32Test extends TestSuite with ChiselUtestTester{
   def tests: Tests = Tests {
-    test("LZC8 should pass") {
+    test("LZC32 should pass") {
       def testcase() = {
-        val p: Int = Random.nextInt(256)
+        val r: Int = Random.nextInt(32)
+        val p:Int = BigInt(r,Random).toInt
         def zeroCount(x: BigInt): Int = {
           var flag = false
-          var a: Int = 7
+          var a: Int =31
           while (!flag && (a >= -1)) {
             flag = ((BigInt(1) << a) & x) != 0
             a = a - 1
           }
-          7 - (a + 1)
+          31 - (a + 1)
         }
+
 //        println("Input = " + p.toBinaryString)
+//        println("Input = " + p.toString)
 //        println("zeroCount = " + zeroCount(p))
-        val z_ex = if (p==0) 0 else 7-zeroCount(p)
+        val z_ex = if (p==0) 0 else 31-zeroCount(p)
         val v_ex = if (p==0) 0 else 1
 //        println("z_ex=" + z_ex)
 //        println("v_ex=" + v_ex)
 
-        testCircuit(new LZC8,
+        testCircuit(new LZC32,
           Seq(chiseltest.internal.NoThreadingAnnotation,
             chiseltest.simulator.WriteVcdAnnotation)) {
-          dut : LZC8 =>
+          dut : LZC32 =>
             dut.io.a.poke(p.U)
 //            println("Z = " + dut.io.z.peek().litValue.toString())
 //            println("V = " + dut.io.v.peek().litValue.toString())
@@ -42,9 +43,12 @@ object LZC8Test extends TestSuite with ChiselUtestTester{
 
         }
       }
-      for( i <- 1 to 8){
+
+      for (i <- 1 to 32) {
         testcase()
       }
+
+
     }
 
   }
