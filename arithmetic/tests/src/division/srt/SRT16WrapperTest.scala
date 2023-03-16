7e719a5fd636dc369bd903ecd528972809e71ac7
Yanqi Yang <shxxyyq0000@qq.com>
2023-03-16 14:07:09 +0800

    fix srt16Wrapper remainder

--- arithmetic/tests/src/division/srt/SRT16WrapperTest.scala
+++ arithmetic/tests/src/division/srt/SRT16WrapperTest.scala
@@ -10,7 +10,7 @@ import scala.util.Random
 object SRT16WrapperTest extends TestSuite with ChiselUtestTester {
   def tests: Tests = Tests {
     test("SRT16Wrapper should pass") {
-      def testcase(width: Int): Unit ={
+      def testcase(width: Int,init:Int): Unit ={
         // parameters
         val radixLog2 = 4
         val n: Int = width
@@ -21,8 +21,10 @@ object SRT16WrapperTest extends TestSuite with ChiselUtestTester {
         val signRandom2: Int = Random.nextInt(2)
         val sign1: Int = if (signRandom1 == 0) -1 else 1
         val sign2: Int = if (signRandom2 == 0) -1 else 1
-        val dividend: BigInt = BigInt(p, Random) * sign1
-        val divisor: BigInt = BigInt(q, Random) * sign2
+//        val dividend: BigInt = BigInt(p, Random) * sign1
+//        val divisor: BigInt = BigInt(q, Random) * sign2
+        val dividend: BigInt = 1227133513 + init
+        val divisor: BigInt = 64842
 
         if(divisor == 0) return
         val quotient_ex = dividend / divisor
@@ -63,15 +65,18 @@ object SRT16WrapperTest extends TestSuite with ChiselUtestTester {
             for (a <- 1 to 1000 if !flag) {
               if (dut.output.valid.peek().litValue == 1) {
                 flag = true
-//                println("%d / %d = %d --- %d".format(dividend, divisor, quotient_ex, remainder_ex))
-//                println("%d / %d = %d --- %d".format(dut.debug.dividend.peek().litValue, dut.debug.divisor.peek().litValue, dut.output.bits.quotient.peek().litValue, dut.output.bits.reminder.peek().litValue))
-//                println("rtl needComputerWidth = %d / %d ".format(dut.debug.needComputerWidth.peek().litValue,needComputerWidth))
-//                println("rtl noguard = %b / %b ".format(dut.debug.noguard.peek().litValue,noguard))
-//                println("rtl guardWidth = %d / %d ".format(dut.debug.guardWidth.peek().litValue,guardWidth))
-//                println("rtl counter = %d / %d ".format(dut.debug.counter.peek().litValue,counter))
-//                println("rtl zeroHeadDividend = %d / %d ".format(dut.debug.zeroHeadDividend.peek().litValue,zeroHeadDividend))
-//                println("rtl leftShiftWidthDividend = %d / %d ".format(dut.debug.leftShiftWidthDividend.peek().litValue,leftShiftWidthDividend))
-//                println("rtl leftShiftWidthDivisor = %d / %d ".format(dut.debug.leftShiftWidthDivisor.peek().litValue, leftShiftWidthDivider))
+                                println("zeroHeadDividend = %d".format(zeroHeadDividend))
+                                println("zeroHeadDivider = %d".format(zeroHeadDivider))
+                                println("needComputerWidth = %d".format(needComputerWidth))
+                                println("guardWidth = %d".format(guardWidth))
+                                println("dividend = %d".format(dividend))
+                                println("leftShiftWidthDividend = %d".format(leftShiftWidthDividend))
+                                println("dividend_shift = %x".format(dividend << leftShiftWidthDividend))
+                println("%d / %d = %d --- %d".format(dividend, divisor, quotient_ex, remainder_ex))
+                //                println("%d / %d = %d --- %d".format(dividend, divider, dut.output.bits.quotient.peek().litValue,
+                //                  (dut.output.bits.reminder.peek().litValue >> zeroHeadDivider) + (if( dividend%2 == 1) 1 else 0 )))
+                println("%d / %d = %d --- %d".format(dividend, divisor, dut.output.bits.quotient.peek().litValue,
+                  dut.output.bits.reminder.peek().litValue))
                 utest.assert(dut.output.bits.quotient.peek().litValue == quotient_ex)
                 utest.assert(dut.output.bits.reminder.peek().litValue == remainder_ex)
               }
@@ -82,7 +87,9 @@ object SRT16WrapperTest extends TestSuite with ChiselUtestTester {
         }
     }
 
-      for (i <- 1 to 20) testcase(32)
+      for (i <- 0 to 20) {
+        testcase(32, i)
+      }
     }
   }
 }
