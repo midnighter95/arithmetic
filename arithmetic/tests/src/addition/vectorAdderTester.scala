4947decfe04a782279f1c74742f634f1b0423a7c
Yanqi Yang <shxxyyq0000@qq.com>
2023-09-01 15:00:15 +0800

    [rtl] vectorAdder8 passed

---
+++ arithmetic/tests/src/addition/vectorAdderTester.scala
@@ -0,0 +1,42 @@
+package addition
+
+import chisel3._
+import chiseltest._
+import utest._
+import scala.util.Random
+
+object vectorAdderTester extends TestSuite with ChiselUtestTester {
+  def tests: Tests = Tests {
+    test("vectorAdder should pass") {
+      def testcase(width: Int): Unit = {
+
+        val a = Random.nextInt(255)
+        val b = Random.nextInt(255)
+        val cin = Random.nextInt(1)
+
+        val z = a + b + cin
+        val overflow = z > 255
+        val z_expect = if(overflow) z-256 else z
+
+        // test
+        testCircuit(
+          new vectorAdder(width),
+          Seq(chiseltest.internal.NoThreadingAnnotation, chiseltest.simulator.WriteVcdAnnotation)
+        ) { dut: vectorAdder =>
+          dut.clock.setTimeout(0)
+          dut.a.poke(a.U)
+          dut.b.poke(b.U)
+          dut.cin.poke(cin.U)
+          dut.z.expect(z_expect.U)
+          dut.cout.expect(overflow.B)
+        }
+      }
+
+
+            for (i <- 1 to 100) {
+              testcase(8)
+            }
+
+    }
+  }
+}
