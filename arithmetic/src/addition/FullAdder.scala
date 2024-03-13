2415e3bae97590be4bd5ba6c751b61cc47a5b36e
Yanqi Yang <shxxyyq0000@qq.com>
2024-03-13 16:16:20 +0800

    add PrefixAdderForCadence

--- arithmetic/src/addition/FullAdder.scala
+++ arithmetic/src/addition/FullAdder.scala
@@ -27,3 +27,14 @@ trait FullAdderWithAssert extends Module {
 
   assert(a +& b + cin === Cat(cout, z))
 }
+trait FullAdderForCadence extends Module {
+  val width: Int
+  //  require(width > 0)
+  val a: UInt = IO(Input(UInt(width.W)))
+  val b: UInt = IO(Input(UInt(width.W)))
+
+  val z = Seq.tabulate(width)(i => IO(Output(Bool())).suggestName(s"z_${i}"))
+
+  val cin:  Bool = IO(Input(Bool()))
+  val cout: Bool = IO(Output(Bool()))
+}
